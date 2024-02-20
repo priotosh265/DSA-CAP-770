1227b552e8794ae46b80fea6cb8aeedaaea59bf6
Priotosh Das <65670862+priotosh265@users.noreply.github.com>
2024-02-20 22:47:21 +0530

    Add files via upload

---
+++ printPrimeNumbers.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int ct=0,n=0,i=1,j=1;  
+    while(n<25)  
+    {  
+        j=1;  
+        ct=0;  
+        while(j<=i)  
+        {  
+            if(i%j==0)  
+            ct++;  
+            j++;   
+        }  
+        if(ct==2)  
+        {  
+            printf("%d ",i);  
+            n++;  
+        }  
+        i++;  
+}  
+
+    return 0;
+}
